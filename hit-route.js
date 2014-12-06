ac7e35dd9b8e18fe6986ca1861e4889e06b7bf34
postazure <gordonmaxc@gmail.com>
2014-12-06 12:45:14 -0800

    first commit

---
+++ hit-route.js
@@ -0,0 +1,45 @@
+var j5 = require("johnny-five");
+var express = require('express');
+
+var myBoard, myLed;
+var app = express()
+
+
+myBoard = new j5.Board();
+
+var ledOn = function() {
+  myLed = new j5.Led(13)
+  myLed.on();
+
+  myBoard.repl.inject({
+    led: myLed
+  });
+}
+
+var ledOff = function() {
+  myLed = new j5.Led(13)
+  myLed.off();
+
+  myBoard.repl.inject({
+    led: myLed
+  });
+}
+
+app.get('/', function (req, res) {
+  res.redirect("/off.html")
+})
+
+app.get('/on', function (req, res) {
+  ledOn();
+  res.send('success')
+})
+
+app.get("/off", function (req, res) {
+  ledOff();
+  res.send('success')
+})
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000);
